497ec71cd9e30947a6e73811a56f10c85298530a
pyramidhead <jasondanielwall@gmail.com>
2021-09-02 17:11:27 -0700

    fixing directory structure mistake

--- messWithTexas.sh
+++ messWithTexas.sh
@@ -5,18 +5,18 @@ sudo mkdir messWithTexas
 
 # enable free vpn to spoof tx ip
 ## install protonvpn cli
-sudo mkdir messWithTexas/vpn
+sudo mkdir vpn
 sudo apt-get install -y dpkg-dev
 sudo apt-get install -y apt-file
 sudo apt-get install -y expect
 sudo apt-get install -y send
 sudo apt-file update
 sudo apt-get update
-sudo wget -O ~/messWithTexas/vpn/protonVpn.deb https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb
-sudo chmod 755 ~/messWithTexas/vpn/protonVpn.deb
-sudo apt-file find ~/messWithTexas/vpn/protonVpn.deb
-sudo dpkg -i ~/messWithTexas/vpn/protonVpn.deb
-sudo apt-get install -y ~/messWithTexas/vpn/protonVpn.deb
+sudo wget -O ~/vpn/protonVpn.deb https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb
+sudo chmod 755 ~/vpn/protonVpn.deb
+sudo apt-file find ~vpn/protonVpn.deb
+sudo dpkg -i ~vpn/protonVpn.deb
+sudo apt-get install -y ~/vpn/protonVpn.deb
 sudo apt-get update
 sudo apt-get install -y protonvpn-cli
 ## authenticate with free credentials
