94ad5dfbcf6ba5d69d05d3748549216dc123b3eb
pyramidhead <jasondanielwall@gmail.com>
2021-09-02 17:14:52 -0700

    still working dependencies

--- messWithTexas.sh
+++ messWithTexas.sh
@@ -9,7 +9,7 @@ sudo mkdir vpn
 sudo apt-get install -y dpkg-dev
 sudo apt-get install -y apt-file
 sudo apt-get install -y expect
-sudo apt-get install -y send
+sudo apt-get install mmh
 sudo apt-file update
 sudo apt-get update
 sudo wget -O ~/vpn/protonVpn.deb https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb
