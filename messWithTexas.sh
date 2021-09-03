b1d7e9abf26e69b6d34f8cd1931a51837f840000
pyramidhead <jasondanielwall@gmail.com>
2021-09-02 18:29:09 -0700

    trying server selection within expect

--- messWithTexas.sh
+++ messWithTexas.sh
@@ -25,6 +25,7 @@ spawn protonvpn-cli login messWithTexas
 expect "Enter your ProtonVPN password:"
 send "3N75Zw!UyRd7@7z\n"
 expect EOF
+protonvpn-cli c US-TX#6
 AUTH
 
 # generate addresses at https://coolgenerator.com/address-generator (is this necessary? the next step generates names and addresses)
