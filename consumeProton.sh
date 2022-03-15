c6d449e0b1b8c1377159fbb64de326f272e2e719
Pyramid Ronin <pyramidhead@users.noreply.github.com>
2022-03-15 11:10:25 -0700

    Update consumeProton.sh
    decision made at a higher architecture level. reverting

--- consumeProton.sh
+++ consumeProton.sh
@@ -1,25 +1,25 @@
 # enable free vpn
 ## install protonvpn cli
-sudo yum install -y dpkg
-# sudo apt-get install -y dpkg-dev
-# sudo apt-get install -y apt-file
-# sudo apt-get install -y expect
-# sudo apt-get install -y mmh
-# sudo apt-get install -y nmh
-# sudo apt-get install -y mailutils-mh
-# sudo apt-file update
-# sudo apt-get update
-wget -O ~/git/consumeProton/protonVpn.deb https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb
-sudo chmod 755 ~/git/consumeProton/protonVpn.deb
-# sudo apt-file find ~/protonVpn.deb
-# sudo dpkg -i ~/protonVpn.deb
-# sudo apt-get install -y ~/protonVpn.deb
-# sudo apt-get update
-# sudo apt-get install -y protonvpn-cli
+sudo mkdir vpn
+sudo apt-get install -y dpkg-dev
+sudo apt-get install -y apt-file
+sudo apt-get install -y expect
+sudo apt-get install -y mmh
+sudo apt-get install -y nmh
+sudo apt-get install -y mailutils-mh
+sudo apt-file update
+sudo apt-get update
+sudo wget -O ~/protonVpn.deb https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb
+sudo chmod 755 ~/protonVpn.deb
+sudo apt-file find ~/protonVpn.deb
+sudo dpkg -i ~/protonVpn.deb
+sudo apt-get install -y ~/protonVpn.deb
+sudo apt-get update
+sudo apt-get install -y protonvpn-cli
 ## authenticate with free credentials
-# /usr/bin/expect << AUTH
-# spawn protonvpn-cli login <yourLoginHere>
-# expect "Enter your ProtonVPN password:"
-# send "<yourPasswordHere>"
-# expect EOF
-# AUTH
+/usr/bin/expect << AUTH
+spawn protonvpn-cli login <yourLoginHere>
+expect "Enter your ProtonVPN password:"
+send "<yourPasswordHere>"
+expect EOF
+AUTH
