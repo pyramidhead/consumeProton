e008cd0a822154545536f001d47c00324e0cc88a
Pyramid Ronin <pyramidhead@users.noreply.github.com>
2022-03-29 13:40:29 -0700

    Update consumeProton.sh
    simplifying

--- consumeProton.sh
+++ consumeProton.sh
@@ -1,6 +1,5 @@
 # enable free vpn
 ## install protonvpn cli
-sudo mkdir vpn
 sudo apt-get install -y dpkg-dev
 sudo apt-get install -y apt-file
 sudo apt-get install -y expect
@@ -8,12 +7,6 @@ sudo apt-get install -y mmh
 sudo apt-get install -y nmh
 sudo apt-get install -y mailutils-mh
 sudo apt-file update
-sudo apt-get update
-sudo wget -O ~/protonVpn.deb https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb
-sudo chmod 755 ~/protonVpn.deb
-sudo apt-file find ~/protonVpn.deb
-sudo dpkg -i ~/protonVpn.deb
-sudo apt-get install -y ~/protonVpn.deb
 sudo apt-get update
 sudo apt-get install -y protonvpn-cli
 ## authenticate with free credentials
